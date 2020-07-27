835f6bcba4d320d033d5caaedfa6cce9099d0dc1
Ricardo <nelsonricardopaltincabrera@gmail.com>
2020-07-27 12:12:23 -0500

    Se agregó pantallas win y lose

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Categorias from './component/categorias';
 import Application from "./component/Application";
 import Tipo from './component/Tipo';
 import Home from './component/Home';
+import win from './component/win';
+import lose from './component/lose';
 import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
 import ImageLoad from "./component/ImageLoad";
 
@@ -17,6 +19,8 @@ function App() {
                 <Route path="/tipo" component={Tipo}/>
                 <Route path="/categorias" component={Categorias}/>
                 <Route path="/game" component={Application}/>
+                <Route path="/win" component={win}/>
+                <Route path="/lose" component={lose}/>
             </div>
         </Router>
     );
---
+++ src/component/win.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+    render(){
+        return(
+ 
+            
+            <div class="container">
+                
+                 <div id="logo">
+                    <img src="assets/win.png" width='200px'height='200px'/> 
+                </div>
+                <Link to="/tipo" class ="parpadea text">
+                    <img  src="assets/start.png" width='250px'height='100px'/>
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default Home;
